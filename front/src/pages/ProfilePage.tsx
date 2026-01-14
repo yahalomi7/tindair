d0de30646e92a1cdfa17f376496ad7c3531f33a4
yahalomi7 <ctahrh123456789@gmail.com>
2026-01-14 13:07:37 +0200

    14.1.26

--- front/src/pages/ProfilePage.tsx
+++ front/src/pages/ProfilePage.tsx
@@ -2,30 +2,96 @@ import Box from "@mui/material/Box";
 import Navbar from "../components/Navbar";
 
 export default function ProfilePage() {
-    return (
-        <div>
-   <Box
-  sx={{
-    minHeight: '500px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }}
->
-  <Box
-    sx={{
-      height: '300px',
-      width: '300px',
-      borderRadius: '50%',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      bgcolor: '#f5f5f5',
-    }}
-  >
-      profile pic 
-  </Box>
-</Box>            <Navbar  />
-        </div>
-    ) 
+  return (
+    <div>
+      <Box
+        sx={{
+          minHeight: '500px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            height: '300px',
+            width: '300px',
+            borderRadius: '50%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            bgcolor: '#f5f5f5',
+          }}
+        >
+          profile pic
+        </Box>
+
+      </Box>
+      <Box
+        sx={{
+          minHeight: '500px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            height: '100px',
+            width: '200px',
+            borderRadius: '10%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            bgcolor: '#f5f5f5',
+            marginRight: '20px',
+          }}
+        > name
+        </Box>
+        <Box
+          sx={{
+            height: '100px',
+            width: '200px',
+            borderRadius: '10%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            bgcolor: '#f5f5f5',
+            marginRight: '20px',
+
+          }}
+        > age
+        </Box>
+        <Box
+          sx={{
+            height: '100px',
+            width: '200px',
+            borderRadius: '10%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            bgcolor: '#f5f5f5',
+            marginRight: '20px',
+
+          }}
+        > yehida
+        </Box>
+            <Box
+          sx={{
+            height: '100px',
+            width: '200px',
+            borderRadius: '10%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            bgcolor: '#f5f5f5',
+            marginRight: '20px',
+
+          }}
+        > base
+        </Box>
+      </Box>
+      <Navbar />
+    </div>
+  )
 }
